68f72d856ed818e7144fb16f0932d493478c79e5
lyxmx <1939623838@qq.com>
2018-01-02 17:11:52 +0800

    git@github.com:lyxmx/DOME175.git

---
+++ src/42徐美娴/Demo.java
@@ -0,0 +1,25 @@
+package XU;
+import java.awt.Frame;
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	//Thread.sleep(5000);
+	//fram.show();
+	Scanner sc=new Scaner(System.in);
+	System.out.println("请输入一个你喜欢的数字");
+	System.out.println("请输入一个你喜欢的数字");
+	System.out.println("请输入一个你喜欢的数字");
+	System.out.println("请输入一个你喜欢的数字");
+	System.out.println("请输入一个你喜欢的数字");
+	System.out.println("请输入一个你喜欢的数字");
+	int b=sc.nextInt();
+	int [] arr;
+	arr=new int[a];
+	for(int i=1;i<arr.length;i++);
+	System.out.println(i+1);
+	
+	}
+
+}
---
+++ src/java/mx.java
@@ -0,0 +1,12 @@
+package java;
+
+public class mx {
+public static void main(Sring[] args){
+System.out.println("Hello175");	
+	
+	
+	
+	
+	
+}
+}
